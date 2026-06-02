a1502c4a9c1f9bac988540fce0c5302f42c658fe
Bachir Uchiwa <bachirdiame61@gmail.com>
2026-06-02 12:42:45 +0000

    02-06-2024 amelioration du code pour navbar

--- components/ui/tab-bar/index.js
+++ components/ui/tab-bar/index.js
@@ -1,3 +1,34 @@
+const DEFAULT_TABS = [
+  {
+    id: 'home',
+    label: 'Accueil',
+    icon: '/assets/icones/home.svg',
+    url: '/pages/index/index',
+    content: 'Bienvenue sur Accueil',
+  },
+  {
+    id: 'explorer',
+    label: 'Explorer',
+    icon: '/assets/icones/boussole.svg',
+    url: '/pages/explorer/index',
+    content: 'Page Explorer',
+  },
+  {
+    id: 'favorites',
+    label: 'Favoris',
+    icon: '/assets/icones/heart.svg',
+    url: '/pages/favorites/index',
+    content: 'Page Favoris',
+  },
+  {
+    id: 'reservations',
+    label: 'R\u00e9servations',
+    icon: '/assets/icones/calendrier.svg',
+    url: '/pages/reservations/index',
+    content: 'Page R\u00e9servations',
+  },
+];
+
 Component({
   properties: {
     activeTab: {
@@ -7,15 +38,46 @@ Component({
 
     tabs: {
       type: Array,
-      value: [],
+      value: DEFAULT_TABS,
+    },
+  },
+
+  data: {
+    currentTab: DEFAULT_TABS[0],
+  },
+
+  lifetimes: {
+    attached() {
+      this.updateCurrentTab();
+    },
+  },
+
+  observers: {
+    'activeTab, tabs': function () {
+      this.updateCurrentTab();
     },
   },
 
   methods: {
-    handleTap(e) {
-      const tab = e.currentTarget.dataset.tab;
+    updateCurrentTab() {
+      const currentTab = this.properties.tabs.find((tab) => tab.id === this.properties.activeTab);
 
-      this.triggerEvent('change', { tab });
+      if (currentTab) {
+        this.setData({ currentTab });
+      }
+    },
+
+    handleTap(e) {
+      const tabId = e.currentTarget.dataset.tab;
+      const tab = this.properties.tabs.find((item) => item.id === tabId);
+
+      if (!tab || tab.id === this.properties.activeTab) return;
+
+      this.triggerEvent('change', { tab: tab.id, url: tab.url });
+
+      if (tab.url) {
+        wx.redirectTo({ url: tab.url });
+      }
     },
   },
 });
